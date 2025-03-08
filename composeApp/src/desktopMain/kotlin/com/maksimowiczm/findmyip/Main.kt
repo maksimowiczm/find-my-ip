82080330dc897aaa556e134d772258e849796a8f
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-08 14:00:57 +0100

    feat(desktop): add app initializer
    - Add `AppInitializer` to execute initialization logic on desktop app startup.
    - Inject `AppInitializer` and run it on app start using `LaunchedEffect`.

--- composeApp/src/desktopMain/kotlin/com/maksimowiczm/findmyip/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/maksimowiczm/findmyip/Main.kt
@@ -1,15 +1,23 @@
 package com.maksimowiczm.findmyip
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.maksimowiczm.findmyip.data.initializer.AppInitializer
 import com.maksimowiczm.findmyip.infrastructure.di.initKoin
 import com.maksimowiczm.findmyip.ui.FindMyIpApp
 import findmyip.composeapp.generated.resources.Res
 import findmyip.composeapp.generated.resources.app_name
 import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
 
 fun main() = application {
     initKoin()
+
+    val appInitializer = koinInject<AppInitializer>()
+    LaunchedEffect(Unit) {
+        appInitializer()
+    }
 
     Window(
         onCloseRequest = ::exitApplication,
