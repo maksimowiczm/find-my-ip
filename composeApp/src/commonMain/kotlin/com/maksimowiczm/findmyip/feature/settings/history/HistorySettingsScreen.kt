405cbd697520c8a300ba98202ce17dc3f69a99b6
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-04-02 15:03:46 +0200

    feat(history-settings): implement shimmer for loading state

--- composeApp/src/commonMain/kotlin/com/maksimowiczm/findmyip/feature/settings/history/HistorySettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/maksimowiczm/findmyip/feature/settings/history/HistorySettingsScreen.kt
@@ -1,11 +1,12 @@
 package com.maksimowiczm.findmyip.feature.settings.history
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,9 +21,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -30,12 +31,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.maksimowiczm.findmyip.infrastructure.di.container
+import com.valentinilk.shimmer.shimmer
 import findmyip.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.stringResource
 import pro.respawn.flowmvi.api.IntentReceiver
@@ -49,37 +51,41 @@ fun HistorySettingsScreen(modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 internal fun HistorySettingsScreen(
     modifier: Modifier = Modifier,
     container: HistorySettingsContainer = container()
 ) = with(container.store) {
     val state by subscribe()
-
-    when (val state = state) {
-        HistorySettingsState.Loading -> Surface { Spacer(Modifier.fillMaxSize()) }
-        is HistorySettingsState.Loaded -> HistorySettingsScreen(
-            state = state,
-            modifier = modifier
-        )
-    }
+    HistorySettingsScreen(
+        state = state,
+        modifier = modifier
+    )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 private fun IntentReceiver<HistorySettingsIntent>.HistorySettingsScreen(
-    state: HistorySettingsState.Loaded,
+    state: HistorySettingsState,
     modifier: Modifier = Modifier
 ) {
-    val onToggle = {
-        val intent = when (state) {
-            is HistorySettingsState.Disabled -> HistorySettingsIntent.EnableHistory
-            is HistorySettingsState.Enabled -> HistorySettingsIntent.DisableHistory
+    val onToggle = remember(state) {
+        {
+            val intent = when (state) {
+                HistorySettingsState.Loading -> null
+                is HistorySettingsState.Disabled -> HistorySettingsIntent.EnableHistory
+                is HistorySettingsState.Enabled -> HistorySettingsIntent.DisableHistory
+            }
+
+            if (intent != null) {
+                intent(intent)
+            }
         }
-        intent(intent)
     }
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val transition = updateTransition(state)
 
     Scaffold(
         modifier = modifier,
@@ -100,38 +106,86 @@ private fun IntentReceiver<HistorySettingsIntent>.HistorySettingsScreen(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
         ) {
             item {
-                Card(
-                    onClick = onToggle,
-                    modifier = Modifier.padding(16.dp),
-                    shape = MaterialTheme.shapes.large,
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                    )
+                transition.Crossfade(
+                    contentKey = { it is HistorySettingsState.Loading }
                 ) {
-                    Row(
-                        modifier = Modifier.padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = stringResource(Res.string.action_use_history),
-                            style = MaterialTheme.typography.bodyLarge
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        Switch(
-                            checked = state is HistorySettingsState.Enabled,
-                            onCheckedChange = {
-                                when (it) {
-                                    true -> intent(HistorySettingsIntent.EnableHistory)
-                                    false -> intent(HistorySettingsIntent.DisableHistory)
-                                }
-                            }
-                        )
+                    when (it) {
+                        HistorySettingsState.Loading -> Card(
+                            onClick = {},
+                            modifier = Modifier
+                                .shimmer()
+                                .padding(16.dp),
+                            shape = MaterialTheme.shapes.large,
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                            )
+                        ) {
+                            ListItem(
+                                headlineContent = {
+                                    Text(
+                                        text = stringResource(Res.string.action_use_history),
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                },
+                                modifier = Modifier.padding(8.dp),
+                                trailingContent = {
+                                    Switch(
+                                        checked = state is HistorySettingsState.Enabled,
+                                        onCheckedChange = {}
+                                    )
+                                },
+                                colors = ListItemDefaults.colors(
+                                    containerColor = Color.Transparent
+                                )
+                            )
+                        }
+
+                        is HistorySettingsState.Loaded -> Card(
+                            onClick = onToggle,
+                            modifier = Modifier.padding(16.dp),
+                            shape = MaterialTheme.shapes.large,
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                            )
+                        ) {
+                            ListItem(
+                                headlineContent = {
+                                    Text(
+                                        text = stringResource(Res.string.action_use_history),
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                },
+                                modifier = Modifier.padding(8.dp),
+                                trailingContent = {
+                                    Switch(
+                                        checked = state is HistorySettingsState.Enabled,
+                                        onCheckedChange = {
+                                            when (it) {
+                                                true -> intent(
+                                                    HistorySettingsIntent.EnableHistory
+                                                )
+
+                                                false -> intent(
+                                                    HistorySettingsIntent.DisableHistory
+                                                )
+                                            }
+                                        }
+                                    )
+                                },
+                                colors = ListItemDefaults.colors(
+                                    containerColor = Color.Transparent
+                                )
+                            )
+                        }
                     }
                 }
             }
 
             when (state) {
+                HistorySettingsState.Loading -> Unit
+
                 HistorySettingsState.Disabled -> item {
                     Column(
                         modifier = Modifier.padding(16.dp),
@@ -216,7 +270,9 @@ private fun LazyListScope.enabledContent(
 
     item {
         Text(
-            modifier = Modifier.padding(horizontal = 16.dp).padding(bottom = 8.dp),
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 8.dp),
             text = stringResource(Res.string.headline_network_type),
             style = MaterialTheme.typography.labelLarge,
             color = MaterialTheme.colorScheme.primary
