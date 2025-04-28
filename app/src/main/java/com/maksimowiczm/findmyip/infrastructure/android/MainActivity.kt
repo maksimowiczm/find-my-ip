369f6f1253c79ef82dc2face2a31adea207f57e3
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-04-28 16:07:57 +0200

    refactor: use AppCompatActivity

--- app/src/main/java/com/maksimowiczm/findmyip/infrastructure/android/MainActivity.kt
+++ app/src/main/java/com/maksimowiczm/findmyip/infrastructure/android/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.maksimowiczm.findmyip.infrastructure.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import com.maksimowiczm.findmyip.ui.FindMyIPApp
 import com.maksimowiczm.findmyip.ui.theme.FindMyIPTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
