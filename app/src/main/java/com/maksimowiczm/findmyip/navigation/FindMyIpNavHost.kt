28bf35d1990cd8505b3d232846ecba706e1bb596
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-02 16:04:55 +0200

    feat: add navigation and transition animations

--- app/src/main/java/com/maksimowiczm/findmyip/navigation/FindMyIpNavHost.kt
+++ app/src/main/java/com/maksimowiczm/findmyip/navigation/FindMyIpNavHost.kt
@@ -1,112 +1,67 @@
 package com.maksimowiczm.findmyip.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.maksimowiczm.findmyip.R
+import com.maksimowiczm.findmyip.ui.motion.materialFadeThroughIn
+import com.maksimowiczm.findmyip.ui.motion.materialFadeThroughOut
 import com.maksimowiczm.findmyip.ui.page.home.HomePage
 import com.maksimowiczm.findmyip.ui.page.settings.SettingsPage
 import com.maksimowiczm.findmyip.ui.page.settings.language.LanguagePage
 import com.maksimowiczm.findmyip.ui.page.settings.notifications.NotificationsPage
 
 @Composable
-fun FindMyIpNavHost(
-    modifier: Modifier = Modifier,
-    appState: AppState = rememberAppNavigationState()
-) {
-    val currentDestination = appState.rememberTopDestination()
-    val navController = appState.navController
+fun FindMyIpNavHost(appNavigationState: AppNavigationState, modifier: Modifier = Modifier) {
+    val navController = appNavigationState.navController
 
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            item(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.Home,
-                        contentDescription = null
-                    )
-                },
-                label = { Text(stringResource(R.string.headline_home)) },
-                selected = currentDestination == Home,
-                onClick = {
-                    navController.popBackStack<Home>(inclusive = false)
-                }
-            )
-            item(
-                icon = {
-                    Icon(
-                        imageVector = Icons.Default.Settings,
-                        contentDescription = null
-                    )
-                },
-                label = { Text(stringResource(R.string.headline_settings)) },
-                selected = currentDestination == Settings,
-                onClick = {
-                    if (currentDestination == Settings) {
-                        navController.popBackStack<SettingsHome>(inclusive = false)
-                    } else {
-                        navController.navigate(Settings) {
+    NavHost(
+        navController = appNavigationState.navController,
+        startDestination = Home,
+        modifier = modifier
+    ) {
+        composable<Home>(
+            enterTransition = { materialFadeThroughIn() },
+            exitTransition = { materialFadeThroughOut() }
+        ) {
+            HomePage()
+        }
+        navigation<Settings>(
+            startDestination = SettingsHome
+        ) {
+            settingsComposable<SettingsHome> {
+                SettingsPage(
+                    onBackgroundServices = {
+                        // TODO
+                    },
+                    onNotifications = {
+                        navController.navigate(NotificationsSettings) {
                             launchSingleTop = true
-                            popUpTo(Settings) {
-                                inclusive = false
-                            }
+                            restoreState = true
+                        }
+                    },
+                    onLanguage = {
+                        navController.navigate(LanguageSettings) {
+                            launchSingleTop = true
+                            restoreState = true
                         }
                     }
-                }
-            )
-        }
-    ) {
-        NavHost(
-            navController = navController,
-            startDestination = Home,
-            modifier = modifier
-        ) {
-            composable<Home> {
-                HomePage()
+                )
             }
-            navigation<Settings>(
-                startDestination = SettingsHome
-            ) {
-                composable<SettingsHome> {
-                    SettingsPage(
-                        onBackgroundServices = {
-                            // TODO
-                        },
-                        onNotifications = {
-                            navController.navigate(NotificationsSettings) {
-                                launchSingleTop = true
-                            }
-                        },
-                        onLanguage = {
-                            navController.navigate(LanguageSettings) {
-                                launchSingleTop = true
-                            }
-                        }
-                    )
-                }
-                composable<NotificationsSettings> {
-                    NotificationsPage(
-                        onBack = {
-                            navController.popBackStack<NotificationsSettings>(inclusive = true)
-                        }
-                    )
-                }
-                composable<LanguageSettings> {
-                    LanguagePage(
-                        onBack = {
-                            navController.popBackStack<LanguageSettings>(inclusive = true)
-                        }
-                    )
-                }
+            settingsComposable<NotificationsSettings> {
+                NotificationsPage(
+                    onBack = {
+                        navController.popBackStack<NotificationsSettings>(inclusive = true)
+                    }
+                )
+            }
+            settingsComposable<LanguageSettings> {
+                LanguagePage(
+                    onBack = {
+                        navController.popBackStack<LanguageSettings>(inclusive = true)
+                    }
+                )
             }
         }
     }
--- app/src/main/java/com/maksimowiczm/findmyip/ui/FindMyIPApp.kt
+++ app/src/main/java/com/maksimowiczm/findmyip/ui/FindMyIPApp.kt
@@ -1,10 +1,67 @@
 package com.maksimowiczm.findmyip.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.maksimowiczm.findmyip.R
 import com.maksimowiczm.findmyip.navigation.FindMyIpNavHost
+import com.maksimowiczm.findmyip.navigation.Home
+import com.maksimowiczm.findmyip.navigation.Settings
+import com.maksimowiczm.findmyip.navigation.SettingsHome
+import com.maksimowiczm.findmyip.navigation.rememberAppNavigationState
 
 @Composable
 fun FindMyIPApp(modifier: Modifier = Modifier) {
-    FindMyIpNavHost(modifier)
+    val appNavigationState = rememberAppNavigationState()
+    val currentDestination = appNavigationState.rememberTopDestination()
+    val navController = appNavigationState.navController
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            item(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = { Text(stringResource(R.string.headline_home)) },
+                selected = currentDestination == Home,
+                onClick = {
+                    navController.popBackStack<Home>(inclusive = false, saveState = true)
+                }
+            )
+            item(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null
+                    )
+                },
+                label = { Text(stringResource(R.string.headline_settings)) },
+                selected = currentDestination == Settings,
+                onClick = {
+                    if (currentDestination == Settings) {
+                        navController.popBackStack<SettingsHome>(inclusive = false)
+                    } else {
+                        navController.navigate(Settings) {
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                }
+            )
+        }
+    ) {
+        FindMyIpNavHost(
+            modifier = modifier,
+            appNavigationState = appNavigationState
+        )
+    }
 }
