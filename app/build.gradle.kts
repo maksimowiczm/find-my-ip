6a017b732634b44cd17006c87b60ed051c29dd40
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-06-30 13:03:29 +0200

    bump version add changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.maksimowiczm.findmyip"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "4.0.1"
+        versionCode = 13
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
